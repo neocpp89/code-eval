df2ffb9428d06d53032c9fc9f05e4f3046bc52b1
Sachith Dunatunga <neocpp@gmail.com>
2015-01-02 03:17:50 -0700

    Finished lowercase text problem (went back to c++).

---
+++ easy/lowercase_text/lt.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <cctype>
+#include <vector>
+
+int main(int argc, char **argv)
+{
+    if (argc != 2) {
+        return 1;
+    }
+
+    std::ifstream f(argv[1], std::ios::binary);
+    f.seekg(0, std::ios::end);
+    size_t s = f.tellg();
+    std::vector<char> v(s);
+    f.seekg(0, std::ios::beg);
+    f.read(&v[0], s);
+    for (size_t i = 0; i < s; i++) {
+        v[i]= std::tolower(v[i]);
+    }
+    std::string str(v.data(), s);
+    std::cout << str;
+    return 0;
+}
