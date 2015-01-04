776cdae492dad4709b97cedf9b0927eeb3cae236
Sachith Dunatunga <neocpp@gmail.com>
2015-01-03 18:06:25 -0700

    Finished the frequency (solved with R, but mem usage is high).

--- hard/the_frequency/tf.r
+++ hard/the_frequency/tf.r
@@ -1,24 +1,22 @@
 #!/usr/bin/env Rscript
 
 argv <- commandArgs(TRUE)
-data <- t(read.table(argv[1], header=FALSE))
-s <- rev(dim(data))
-v <- 1:s[1]
-# print(data)
-#d <- t(data)
-invisible(gc(FALSE))
-# print(d)
+ff <- argv[1]
+con <- file(description=ff, open="r")
 # X11()
-for (i in v) {
-    t <- 1:s[2]
-    dr <- data[,i]
+while ((ssl <- length(std <- scan(con, nlines=1, quiet=TRUE))) > 0) {
+    # print(std)
+    # print(ssl)
+    s = ssl
+    t <- 1:ssl
+    dr <- std
     eq.out = lm(dr ~ t)
     c = coefficients(eq.out)
     dr <- (dr - c[1] - c[2]*t)
     # print(dr)
     fdr <- fft(dr)
-    p <- abs(fdr[1:s[2]/2])
-    f <- 1:s[2]/2
+    p <- abs(fdr[1:ssl/2])
+    f <- 1:ssl/2
     # plot(f, p)
     # plot(t, dr)
     domfreq <- which(p == max(p))
