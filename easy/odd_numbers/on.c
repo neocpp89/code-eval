b810d79f0e6a2b9158213e5435846db7df41a23c
Sachith Dunatunga <neocpp@gmail.com>
2015-01-19 12:28:28 -0500

    Finished odd numbers.

---
+++ easy/odd_numbers/on.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    int i;
+    for (i = 1; i <= 99; i += 2) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
---
+++ easy/odd_numbers/on.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    for (size_t i = 1; i <= 99; i += 2) {
+        std::cout << i << '\n';
+    }
+    return 0;
+}
